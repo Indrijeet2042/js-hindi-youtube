8722887f6a28d052f3472e699ad4c934bacbf0ad
Indrijeet Kumar <kumarindrijeet2042@gmail.com>
2025-10-30 11:46:26 +0000

    02_dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version
+
+alert(3+2)
+
+console.log(3+3); console.log("Indrijeet") //Not a good way to write code. Should be written in a seprate line.
+console.log(3+
+    3
+);//Also not a good practice. 
+
+// The most important things in coding is readability.
+
+//Refer the documentation NDN by Morzilla. The original documentation is hosted by ECMA
+
+let name = "Indrijeet"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//Primitive dataType
+
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+console.log(typeof "Indrijeet") //string
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+console.log(typeof age) //number
+
