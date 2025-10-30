36c951e88ee881eaa14e719d7c5f166754a002d9
Indrijeet Kumar <kumarindrijeet2042@gmail.com>
2025-10-30 11:31:13 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 133444
+let accountEmail = "indrijeet2042@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+// var is an old method of declaring a variable in JavaScript and the modern way is 
+// using let keyword.
+
+
+accountEmail = "indrijeetprofessional@gmail.com"
+accountPassword = "09876"
+accountCity = "Bengaluru"
+
+// veriables can also be declared directly without mentioning type but it's not a good practice. 
+
+// So, basically only using const and let we can declare everything.  
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity])
